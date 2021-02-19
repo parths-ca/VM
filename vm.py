1cd6915b618643eff6e24cea7e85e1b794083512
Parth Shah <parthshahca468@gmail.com>
2021-02-18 22:10:41 -0500

    Please enter the commit message for your changes. Lines starting

---
+++ vm.py
@@ -0,0 +1,10 @@
+a = 55
+b = 48
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print("Addition is : ", addition)
+print("Subtracting is : ", subtraction)
+print("Multiplication is : ", multiplication)
+print("Division : ", division)
